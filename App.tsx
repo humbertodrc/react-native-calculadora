9d2e05759ca34e5e4883e0737b0d6f286d648304
Humberto Rivero <humbertorivero9@gmail.com>
2023-12-22 10:49:15 -0300

    refactor: App component and add CalculadoraScreen and appTheme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import {Text, View} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import CalculadoraScreen from './src/screens/CalculadoraScreen';
+import {styles} from './src/theme/appTheme';
 
 const App = () => {
   return (
-    <View>
-      <Text>Calculadora</Text>
-    </View>
+    <SafeAreaView style={styles.fondo}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <CalculadoraScreen />
+    </SafeAreaView>
   );
 };
 
---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+const CalculadoraScreen = () => {
+  return (
+    <View>
+      <Text style={styles.texto}>CalculadoraScreen</Text>
+    </View>
+  );
+};
+
+export default CalculadoraScreen;
